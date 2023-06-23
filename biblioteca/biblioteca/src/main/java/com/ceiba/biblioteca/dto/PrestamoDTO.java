08eeefd4db155f8590631a1c9e470a84380e1e61
yeris25 <123104349+yeris25@users.noreply.github.com>
2023-06-23 07:10:15 -0500

    Add files via upload

---
+++ biblioteca/biblioteca/src/main/java/com/ceiba/biblioteca/dto/PrestamoDTO.java
@@ -0,0 +1,4 @@
+package com.ceiba.biblioteca.dto;
+
+public class PrestamoDTO {
+}
---
+++ biblioteca/biblioteca/src/main/java/com/ceiba/biblioteca/dto/PrestamoErrorDTO.java
@@ -0,0 +1,13 @@
+package com.ceiba.biblioteca.dto;
+
+public class PrestamoErrorDTO extends PrestamoDTO {
+    private String mensaje;
+
+    public String getMensaje() {
+        return mensaje;
+    }
+
+    public void setMensaje(String mensaje) {
+        this.mensaje = mensaje;
+    }
+}
